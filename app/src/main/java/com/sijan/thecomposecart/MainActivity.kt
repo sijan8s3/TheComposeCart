d5805a5be0a00b7edc8435418c200b1d8d77674a
Sijan <sijan@greatbear.tech>
2023-02-26 14:26:36 +0545

    initial commit

---
+++ app/src/main/java/com/sijan/thecomposecart/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.sijan.thecomposecart
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sijan.thecomposecart.ui.theme.TheComposeCartTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TheComposeCartTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!",
+    fontSize = 30.sp,
+    color = Color.Blue)
+
+    Text(text = "Other Texts!",
+    fontSize = 30.sp,
+    color = Color.Blue)
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TheComposeCartTheme {
+        Greeting("Android")
+    }
+}
